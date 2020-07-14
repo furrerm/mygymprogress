41e35472cea2be59ffce2d12d99941fb6e850f55
furrerm <markus.furrer@gmail.com>
2020-07-14 20:51:30 +0200

    cors policy headers added

--- src/app/workoutoverview/workout/workouts.service.ts
+++ src/app/workoutoverview/workout/workouts.service.ts
@@ -32,9 +32,17 @@ export class WorkoutsService {
   }
 
   getFiles(): Observable<File> {
+    const httpOptions1 = {
+      headers: new HttpHeaders()
+    };
+    // var this.http = require('https-proxy-agent');
+    httpOptions1.headers.append('Access-Control-Allow-Origin', '*');
+    httpOptions1.headers.append('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // httpOptions.headers.append('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    httpOptions1.headers.append('Access-Control-Allow-Credentials', 'true');
     const urlLocal = this.constant.baseAppUrl + 'workout-service/get-workout-images';
     const result: Observable<any> = this.http
-      .get(urlLocal, { responseType: 'blob' });
+      .get(urlLocal, { headers: httpOptions1.headers, responseType: 'blob' });
     return result;
   }
 }
