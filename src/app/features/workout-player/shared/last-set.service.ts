092ff72de4f150a35cc307f4180d63472d0f944f
furrerm <markus.furrer@gmail.com>
2021-02-07 17:29:00 +0100

    userId is now considered by exerciseSet reading

--- src/app/features/workout-player/shared/last-set.service.ts
+++ src/app/features/workout-player/shared/last-set.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {ConstantsService} from '../../../core/services/constants.service';
 import {ExerciseDTO} from '../../../core/model/swagger-model/exerciseDTO';
@@ -8,14 +8,15 @@ import {Observable} from 'rxjs';
   providedIn: 'root'
 })
 export class LastSetService {
-  constructor(private http: HttpClient,
-              private constant: ConstantsService) {
+  constructor(
+    private http: HttpClient,
+    private constant: ConstantsService) {
   }
 
   getSets(exerciseIds: number[]): Observable<ExerciseDTO[]> {
     const endpoint = 'exercise-set/get-sets';
     const endpointURL = this.constant.baseAppUrl + endpoint;
-    const params = new HttpParams().set('exerciseIds', exerciseIds.toString());
-    return (this.http.get<ExerciseDTO[]>(endpointURL, { params }));
+    const params = new HttpParams().set('exerciseIds', exerciseIds.toString()).set('userId', String(this.constant.getUser.id));
+    return (this.http.get<ExerciseDTO[]>(endpointURL, {params}));
   }
 }
